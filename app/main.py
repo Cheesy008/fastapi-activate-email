39f999fea023e34941d95acacb36d8f1a67a367e
Akim Abdulaev <mr.world008@gmail.com>
2021-01-23 20:14:57 +0600

    allow all methods and headers

--- app/main.py
+++ app/main.py
@@ -19,8 +19,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:8080"],
     allow_credentials=True,
-    allow_methods=[""],
-    allow_headers=[""],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
